44cdfbf8ec7bf41adf5a331fddb45d9d782afd1c
MD MEHEDI HASHAN MOLLA <mdmehedihashanmolla@gmail.com>
2024-12-27 20:41:50 +0530

    Adding Keytakeways note

---
+++ Functions/Note.Keytakeways.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/*
+
+Functions are essential for defining logic and interacting with smart contracts.
+
+Modifiers allow you to reuse and centralize access control and validation logic.
+
+Pure functions only depend on input and do not alter or read from the blockchain state.
+
+View functions can read but not alter the state, making them useful for querying data.
+
+Fallback functions help handle incoming Ether or unrecognized function calls.
+
+Restricted access ensures that only authorized users can interact with certain contract functions, improving security.
+*/
